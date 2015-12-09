d7e0c8d75644c5ac77f010bac8af3e016f3bc28d
Leo Lu <leo@LEOPC.eomr.local>
2015-12-09 15:50:00 +0800

    add function description

--- R/save_html.r
+++ R/save_html.r
@@ -1,5 +1,20 @@
-save_html <- function (response, filename='page_output.html'){
+#' Save http text to file
+#'
+#' @param html_text http text
+#' @param filename File name to save
+#'
+#' @export
+#'
+#' @examples
+#' res <- httr::GET("http://google.com/")
+#' html_text <- httr::content(res, "text")
+#' save_html(html_text)
+#'
+save_html <- function (html_text, filename='./page_output/page_output.html'){
+  dir.create("page_output", showWarnings = F)
+
   f <- file(filename, encoding = "UTF-8")
-  writeLines(response, f)
+  writeLines(html_text, f)
   close(f)
+  invisible(NULL)
 }
